271ef536aea875dc866481b93d5364b4b02703c1
George Giannopoulos <74068914+Giannou3250@users.noreply.github.com>
2022-01-26 00:09:19 -0500

    Create Inorder.cs

---
+++ Inorder.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    List<int> l1 = new List<int>();
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        if (root == null)
+        {
+            return l1;
+        }
+        InorderTraversal(root.left);
+        
+        l1.Add(root.val);
+        
+        InorderTraversal(root.right);
+        
+        return l1;
+    }
+}
