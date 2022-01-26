42f61ce609f9552ecf46e2b5cd87479bbae701a7
George Giannopoulos <74068914+Giannou3250@users.noreply.github.com>
2022-01-25 23:56:04 -0500

    Update TwoSum.cs

--- TwoSum.cs
+++ TwoSum.cs
@@ -1,3 +1,12 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+Algorithm - The way I went about coding this problem was by using nested for loops to iterate over 
+            over N elements N times. This way we can check every possible addition value combination 
+            of every element in the array. Make sure that when iterating the loop if i = j do nothing
+            else you can add index values. If inicies I and J add up to the target value then we can 
+            return those indicies in the array.
+*/
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         
