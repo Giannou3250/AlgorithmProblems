835741188c17eed64b50e3d3ffe6d40080bfe5f3
George Giannopoulos <74068914+Giannou3250@users.noreply.github.com>
2022-01-26 00:06:27 -0500

    Update MedianArray.cs

--- MedianArray.cs
+++ MedianArray.cs
@@ -1,3 +1,6 @@
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+*/
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) 
     {
